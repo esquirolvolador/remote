784d501e01404ef292a4821fed77de64b2157eaa
Slopezdiez <slopezdiez@gmail.com>
2022-08-30 18:15:37 +0200

    Text persistence

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -1,9 +1,17 @@
 package repository;
 
+import model.FlowerShop;
 import model.products.Product;
 import model.Ticket;
 
+import java.io.IOException;
+
 public interface Repository {
-    void saveProduct(Product product);
+
+
+    void saveShop(FlowerShop shop);
+
     void saveTicket(Ticket ticket);
+
+    FlowerShop loadShop() throws ClassNotFoundException, IOException;
 }
--- src/main/java/repository/TextFileRepository.java
+++ src/main/java/repository/TextFileRepository.java
@@ -1,16 +1,40 @@
 package repository;
 
+import model.FlowerShop;
 import model.Ticket;
-import model.products.Product;
 
-public class TextFileRepository implements Repository{
+import java.io.*;
+
+public class TextFileRepository implements Repository {
     @Override
-    public void saveProduct(Product product) {
+    public void saveShop(FlowerShop shop) {
 
+        try {
+
+            FileOutputStream fileOutputStream
+                    = new FileOutputStream("yourfile.txt");
+            ObjectOutputStream objectOutputStream
+                    = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(shop);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void saveTicket(Ticket ticket) {
 
+    }
+
+    public FlowerShop loadShop() throws ClassNotFoundException, IOException {
+        FileInputStream fileInputStream
+                = new FileInputStream("yourfile.txt");
+        ObjectInputStream objectInputStream
+                = new ObjectInputStream(fileInputStream);
+        FlowerShop shop = (FlowerShop) objectInputStream.readObject();
+        objectInputStream.close();
+        return shop;
     }
 }
